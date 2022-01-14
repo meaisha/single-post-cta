3f59a43718a88305d4aa86fff6a4a15075201b12
Aisha Agivetova <aisha.agivetova@yorkandchapel.com>
2022-01-13 22:08:37 -0500

    Registers a sidebar, Displays area on single posts, Applying filters for loading stylesheet

--- app/public/wp-content/plugins/single-post-cta/single-post-cta.php
+++ app/public/wp-content/plugins/single-post-cta/single-post-cta.php
@@ -18,9 +18,42 @@ if ( !defined( 'ABSPATH' ) ) {
  * Load stylesheet
  */
 function spc_load_stylesheet() {
-  wp_enqueue_style( 'spc-stylesheet', plugin_dir_url(__FILE__) .'spc-styles.css' );
+  if( apply_filters( 'spc_load_styles', true )) {
+    if( is_single() )
+      p_enqueue_style( 'spc-stylesheet', plugin_dir_url(__FILE__) .'spc-styles.css' );
+  }
+  
 }
+
+// Uncomment the following line to disable stylesheet
+//add_filter( 'spc_load_styles', __return_false);
 
 
 // Hook stylesheet
 add_action( 'wp_enqueue_scripts', 'spc_load_stylesheet' );
+
+function spc_register_sidebar() {
+  register_sidebar( array(
+    'name'          => __( 'Single Post CTA Sidebar', 'spc' ),
+    'id'            => 'spc-sidebar',
+    'description'   => __( 'Displays widget area on sigle posts', 'spc' ),
+    'before_widget' => '<div class="widget spc">',
+    'after_widget'  => '</div>',
+    'before_title'  => '<h2 class="widgettitle spc-title">',
+    'after_title'   => '</h2>',
+  ) );
+}
+
+// Hook sidebar
+add_action( 'widgets_init', 'spc_register_sidebar' );
+
+// Display sidebar on sigle posts
+function spc_display_sidebar( $content ) {
+  if( is_single() ) {
+    dynamic_sidebar( 'spc-sidebar' );
+  }
+  return $content;
+}
+
+// Add dynamic sidebar
+add_filter( 'the_content', 'spc_display_sidebar' );
